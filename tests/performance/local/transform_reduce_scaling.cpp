9f2348432617d00d6fa199514fe5aff96f3a6844
Anushi Maheshwari <anushimaheshwari95@gmail.com>
2018-02-27 09:59:36 +0530

    Update transform_reduce_scaling.cpp
    Change std::rand() to a better PRNG generator std::mt19937.

--- tests/performance/local/transform_reduce_scaling.cpp
+++ tests/performance/local/transform_reduce_scaling.cpp
@@ -1,3 +1,4 @@
+{
 //  Copyright (c) 2014 Grant Mercer
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -19,6 +20,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 int test_count = 100;
+std::mt19937 gen(std::random_device{}());
 
 struct Point
 {
@@ -29,7 +31,7 @@ struct Point
 void measure_transform_reduce(std::size_t size)
 {
     std::vector<Point> data_representation(size,
-        Point{double(std::rand()), double(std::rand())});
+        Point{double(gen()), double(gen())});
 
     // invoke transform_reduce
     double result =
@@ -48,7 +50,7 @@ void measure_transform_reduce(std::size_t size)
 void measure_transform_reduce_old(std::size_t size)
 {
     std::vector<Point> data_representation(size,
-        Point{double(std::rand()), double(std::rand())});
+        Point{double(gen()), double(gen())});
 
     //invoke old reduce
     Point result =
